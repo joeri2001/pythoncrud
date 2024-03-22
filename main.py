46a2287d64024ff3335d9d3b74bbce4b359bbd4d
joeri2001 <joeri2001@proton.me>
2024-03-22 13:05:37 +0100

    protected all routes

--- main.py
+++ main.py
@@ -31,26 +31,26 @@ async def read_stories(token: str = Depends(verify_token)) -> dict:
   return db
 
 @app.post("/stories/")
-async def create_story(story: Story) -> Story:
+async def create_story(story: Story,  token: str = Depends(verify_token)) -> Story:
   db[story.id] = story
   return story
 
 @app.put("/stories/{story_id}")
-async def update_story(story_id: int, story: Story) -> Story:
+async def update_story(story_id: int, story: Story,  token: str = Depends(verify_token)) -> Story:
   db[story_id] = story
   return story
 
 @app.delete("/stories/{story_id}")
-async def delete_story(story_id: int) -> None:
+async def delete_story(story_id: int,  token: str = Depends(verify_token)) -> None:
   return db.pop(story_id, None)
 
 @app.post("/stories/{story_id}/tasks/")
-async def create_task(story_id: int, task: Task) -> Task:
+async def create_task(story_id: int, task: Task,  token: str = Depends(verify_token)) -> Task:
   db[story_id].tasks.append(task)
   return task
 
 @app.put("/stories/{story_id}/tasks/{task_id}")
-async def update_task(story_id: int, task_id: int, task: Task) -> Optional[Task]:
+async def update_task(story_id: int, task_id: int, task: Task,  token: str = Depends(verify_token)) -> Optional[Task]:
   for i, existing_task in enumerate(db[story_id].tasks):
     if existing_task.id == task_id:
       db[story_id].tasks[i] = task
@@ -58,6 +58,6 @@ async def update_task(story_id: int, task_id: int, task: Task) -> Optional[Task]
   return None
 
 @app.delete("/stories/{story_id}/tasks/{task_id}")
-async def delete_task(story_id: int, task_id: int) -> None:
+async def delete_task(story_id: int, task_id: int,  token: str = Depends(verify_token)) -> None:
   db[story_id].tasks = [task for task in db[story_id].tasks if task.id != task_id]
   return None
